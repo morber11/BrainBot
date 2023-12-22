dbeb768dceb153b489615cda1edeedc0d291c6bc
morber <evilorber@gmail.com>
2023-12-22 13:36:07 +0000

    fix packages, add in basic commands

---
+++ src/functions/handlers/command-handler.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const { REST }  = require('@discordjs/rest');
+const { Routes }  = require('discord-api-types/v9');
+
+module.exports = (client) => {
+    client.handleCommands = async() => {
+        const commandFolders = fs.readdirSync('./src/commands');
+        
+        for (const folder of commandFolders) {
+            const commandFiles = fs
+                .readdirSync(`./src/commands/${folder}`)
+                .filter(file => file.endsWith('.js'));
+                
+            const { commands, commandArray } = client;
+            for (const file of commandFiles) {
+                const command = require(`../../commands/${folder}/${file}`);
+
+                commands.set(command.data.name, command);
+                commandArray.push(command.data.toJSON());
+
+            }
+        }
+
+        const clientId = '713026155929665569';
+        //const guildId = '432933197001523213';
+        const rest = new REST({ version: '9'}).setToken(process.env.BOT_TOKEN);
+
+        try {
+            console.log("Started refreshing application (/) commands.");
+
+            await rest.put(Routes.applicationCommands(clientId), {
+                body: client.commandArray,
+            });
+
+            console.log("Succesfully refreshed application (/) commands.");
+        } catch (err) {
+            console.error(err);
+        }
+    };
+}
