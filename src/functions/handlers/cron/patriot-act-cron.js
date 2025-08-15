33ac4cdb51504b43d1056ccf70e9c2d95c14273b
morber <4497491+morber11@users.noreply.github.com>
2025-08-15 15:22:15 +0100

    move delay to start of cron job

--- src/functions/handlers/cron/patriot-act-cron.js
+++ src/functions/handlers/cron/patriot-act-cron.js
@@ -3,6 +3,10 @@ const CONSTANTS = require('../../../utils/constants.js');
 
 const patriotAct = (client) => new cron.CronJob(CONSTANTS.CRON.PATRIOT_ACT, async () => {
     try {
+        // add a small delay so the bot doesn't automatically post it at the exact time - give people some time to react
+        const delay = CONSTANTS.CRON.PATRIOT_ACT_DELAY_PERIOD;
+        await new Promise(resolve => setTimeout(resolve, delay));
+        
         for (const guild of client.guilds.cache.values()) {
             // we have to do this because of funky behaviour with .find() always finding bot/bots instead
             // manually force priority on these special channels
@@ -24,10 +28,6 @@ const patriotAct = (client) => new cron.CronJob(CONSTANTS.CRON.PATRIOT_ACT, asyn
             }
 
             if (targetChannel) {
-                // add a small delay so the bot doesn't automatically post it at the exact time - give people some time to react
-                const delay = CONSTANTS.CRON.PATRIOT_ACT_DELAY_PERIOD;
-                await new Promise(resolve => setTimeout(resolve, delay));
-                
                 await targetChannel.send('o7');
             } else {
                 console.log('No suitable channel found in', guild.name);
