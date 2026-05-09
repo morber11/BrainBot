9b55d8de0fa983f0075e9209eae856179900bc0e
morber <4497491+morber11@users.noreply.github.com>
2026-05-09 18:13:56 +0100

    fix async issues in decerement-despair-cron

--- src/functions/handlers/cron/decrement-despair-cron.js
+++ src/functions/handlers/cron/decrement-despair-cron.js
@@ -1,25 +1,40 @@
 const cron = require('cron');
 const memberService = require('../../../services/member-service.js');
 const CONSTANTS = require('../../../utils/constants.js');
+const logger = require('../../../utils/logger.js');
 
-const decrementDespair = new cron.CronJob(CONSTANTS.CRON.HANDLE_DESPAIR, async () => {
+const onTick = async () => {
     const members = await memberService.findAll();
 
-    members.forEach(async (member) => {
-        const { despairCount, id } = member;
+    for (const member of members) {
+        try {
+            const { despairCount, id } = member;
 
-        if (despairCount > 0) {
-            // this is some sort of insane syntax but it works anyway
-            await member.increment({ despairCount: CONSTANTS.POINT_VALUES.DESPAIR_DECREMENT });
-        }
+            if (despairCount > 0) {
+                // this is some sort of insane syntax but it works anyway
+                await member.increment({ despairCount: CONSTANTS.POINT_VALUES.DESPAIR_DECREMENT });
+            }
 
-        if (despairCount < 0) {
-            await memberService.update(id, {
-                despairCount: 0,
-                updatedAt: new Date(),
-            });
+            if (despairCount < 0) {
+                await memberService.update(id, {
+                    despairCount: 0,
+                    updatedAt: new Date(),
+                });
+            }
+        } catch (err) {
+            logger.error('Failed to process member in decrement despair cron', { memberId: member && member.id, err });
         }
-    });
+    }
+};
+
+// needed for async issues with onTick, 
+// and also allows us to export the cron job 
+// instance for testing
+const decrementDespair = cron.CronJob.from({
+    cronTime: CONSTANTS.CRON.HANDLE_DESPAIR,
+    onTick,
+    start: false,
+    waitForCompletion: true,
 });
 
 module.exports = decrementDespair;
