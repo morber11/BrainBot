c8ca63bbe264760dd6f853200adaee94e6a87a4e
morber <4497491+morber11@users.noreply.github.com>
2026-08-13 13:29:00 +0100

    cron tests should only test if production cron jobs have started

--- src/tests/commands/functions/handlers/cron-handler.test.js
+++ src/tests/commands/functions/handlers/cron-handler.test.js
@@ -5,17 +5,20 @@ describe('handleCrons', () => {
     let mockClient;
     let mockDecrementDespair;
     let mockPatriotAct;
+    let mockReminderPoller;
     let mockDebug;
     let cronHandler;
 
     beforeEach(() => {
         mockDecrementDespair = { start: sinon.stub() };
         mockPatriotAct = { start: sinon.stub() };
+        mockReminderPoller = { start: sinon.stub() };
         mockDebug = { start: sinon.stub() };
 
         cronHandler = proxyquire('../../../../functions/handlers/cron-handler', {
             './cron/decrement-despair-cron': mockDecrementDespair,
             './cron/patriot-act-cron': sinon.stub().returns(mockPatriotAct),
+            './cron/reminder-poller': sinon.stub().returns(mockReminderPoller),
             './cron/debug-cron': sinon.stub().returns(mockDebug),
         });
 
@@ -38,11 +41,12 @@ describe('handleCrons', () => {
         };
     });
 
-    it('should start all cron jobs', async () => {
+    it('should start the production cron jobs', async () => {
         cronHandler(mockClient);
         await mockClient.handleCrons();
         sinon.assert.calledOnce(mockDecrementDespair.start);
         sinon.assert.calledOnce(mockPatriotAct.start);
+        sinon.assert.calledOnce(mockReminderPoller.start);
         sinon.assert.notCalled(mockDebug.start);
     });
 });
